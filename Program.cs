f3b80d321b5484242579a78d2158b954da9ed0ba
margarita <volnyakovamsr92@gmail.com>
2023-01-22 21:10:35 +0700

    домашняя работа 5

--- Program.cs
+++ Program.cs
@@ -36,7 +36,7 @@ long GetPower (int A, int B)
     }
     return Power;
 }
-*/
+
 
 //Напишите программу, которая задает массив из 8 элементов и выводит их на экран.
 int[] Array = GetArray(8);
@@ -52,3 +52,81 @@ int[] GetArray (int size)
     }
     return Array;
 }
+*/
+
+
+
+
+
+//домашняя работа  5
+
+//задайте массив заполненный случайными полопжительными трехзначными числами.
+//напишите программу,которая покажет количество четных чисел в массиве.
+/*
+int[] Array = GetArray(10, 100, 1000);
+Console.WriteLine($"[{String.Join(", ",Array)}]");
+int sum = 0;
+foreach (int el in Array)
+{
+    if(el % 2 == 0){
+        sum += 1;
+    }
+}
+Console.WriteLine($"Четных чисел в массиве {sum}");
+
+int[] GetArray (int size, int minValue, int maxValue){
+    int[] res = new int[size];
+    for(int i = 0; i < size; i++){
+        res[i] = new Random().Next(minValue, maxValue );
+    }
+    return res;
+}
+*/
+//задайте одномерный массив,заполненный случайными числами. Найдите сумму элементов,стоящих на нечетных индексах.
+int[] GetArray1 (int size)
+{
+    int[] Arr = new int[size];
+    for(int i = 0; i < size; i++)
+{
+    Arr[i] = new Random().Next(-10, 11);
+}
+return Arr;
+}
+
+/*
+int[] Array1 = GetArray1 (15);
+Console.WriteLine($"[{String.Join(", ",Array1)}]");
+int result = 0;
+for(int i = 0; i < Array1.Length; i++)
+{
+   
+    if(i % 2 != 0){
+        result = Array1[i] + result;
+    }
+}
+
+
+Console.WriteLine(result);
+*/
+
+//Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент,второй  и предпоследний и т.д. Результат запишите в новый массив.
+int[] Array2 = GetArray1(5);
+Console.WriteLine($"[{String.Join(", ",Array2)}]");
+int Length = Array2.Length;
+
+
+int size = Length / 2 + Length % 2;
+int[] NewArray2 = new int[size];
+for(int i = 0;  i < size;  i++) 
+    {
+       
+      
+       NewArray2[i] = Array2[i] + Array2[Length - i - 1 ];
+       if(i > size / 2){
+        NewArray2[i] = Array2[i];
+
+       }
+    }
+    
+Console.WriteLine($"[{String.Join(", ",NewArray2)}]");
+  
