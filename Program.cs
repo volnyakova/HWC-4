5baba1e764c9fd854eb52826dcee214b9decc68f
margarita <volnyakovamsr92@gmail.com>
2023-02-06 11:48:12 +0700

    ДЗ 9

--- Program.cs
+++ Program.cs
@@ -377,7 +377,7 @@ void GetArrayNew(int[,] array)
     }
 }
 
-*/
+
 
 
 // -------------Домашняя работа 8----------------
@@ -549,6 +549,74 @@ int[,,] Get3dMassive(int size1, int size2, int size3)
     }
     return array;
 }
+*/
 
+
+
+
+//-----------------------домашняя работа 9--------------------------
+
+//Выполнить с помощью рекурсии.Задайте значение N. Напишите программу,
+//которая выведет все натуральные числа в промежутке от N до 1.
+
+Console.Write("Введи число: ");
+int N  = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(PrintNumbers(N, 1));
+
+// ------------------Метод------------------
+string PrintNumbers(int start, int end){
+    if(start == end) return start.ToString();
+    return (start + " " + PrintNumbers(start - 1, end));
+}
+
+
+// Выполнить с помощью рекурсии.Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.WriteLine("Введите число  N"); 
+int n = int.Parse(Console.ReadLine()!); 
+ 
+Console.WriteLine("Введите число  M"); 
+int m = int.Parse(Console.ReadLine()!); 
+
+Console.WriteLine(GetSum(n, m));
+
+//---------метод----------------
+int GetSum(int n, int m)
+{
+    if(n == m) return n;
+    return n + GetSum(n + 1, m);
+}
+
+
+
+
+// Выполнить с помощью рекурсии.
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+
+int m1 = 1; 
+int n1 = 2; 
+Console.WriteLine(ack(m1, n1));
+
+int ack(int m, int n)
+{
+    if(m == 0 )
+    {
+        return n +1;
+    }
+    else if((m > 0) && (n == 0))
+    {
+        return ack(m - 1, 1);
+    }
+    else if((m > 0) && (n > 0))
+    {
+        return ack(m - 1, ack(m, n -1));
+    }
+    else return n+1;
+
+}
 
     
